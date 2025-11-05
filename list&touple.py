f9eab3231e9d620a998d146ad6fbf473c4df7a4f
Nitesh Mishra <niteshmishracs1404@gmail.com>
2025-11-05 18:53:26 +0530

    complete

--- list&touple.py
+++ list&touple.py
@@ -1,26 +1,59 @@
-marks = [85, 90, 78, 92, 88]
-total_obtained_marks = sum(marks)
-print("The total marks are:", total_obtained_marks)
+# marks = [85, 90, 78, 92, 88]
+# total_obtained_marks = sum(marks)
+# print("The total marks are:", total_obtained_marks)
 
 
-list2 = ["apple", "banana", "cherry", "date", "elderberry", "fig", "grape"]
-list2.append("honeydew")
-print("Updated list:", list2)
+# list2 = ["apple", "banana", "cherry", "date", "elderberry", "fig", "grape"]
+# list2.append("honeydew")
+# print("Updated list:", list2)
 
-list3 = [12, 45, 23, 67, 34, 89, 90]
-list3.sort()
-print("Sorted list:", list3)
+# list3 = [12, 45, 23, 67, 34, 89, 90]
+# list3.sort()
+# print("Sorted list:", list3)
 
-# To sort in descending order
-list3.sort(reverse=True)
-print("Sorted list in descending order:", list3)
+# # To sort in descending order
+# list3.sort(reverse=True)
+# print("Sorted list in descending order:", list3)
 
-list4 = [17, 14, 89, 23, 45, 67, 34]
-list4.reverse()
-print("Reversed list:", list4)
+# list4 = [17, 14, 89, 23, 45, 67, 34]
+# list4.reverse()
+# print("Reversed list:", list4)
 
-list4.insert(2, 100)
-list4.remove(89)
-list4.pop(4)
-print("List after insertion:", list4)
+# list4.insert(2, 100)
+# list4.remove(89)
+# list4.pop(4)
+# print("List after insertion:", list4)
 
+
+
+
+
+
+# Now started tuple
+
+# note = string and tuple are immutable means we cant change them but we can change list...
+
+
+# tuple1 = (10, 20, 30, 40, 50)
+# tuple1.index(30)  # it will return the index of 30
+# print("The index of 30 in tuple1 is:", tuple1.index(30))
+
+# tuple1.count(20)  # it will return how many times 20 is present in the tuple
+# print("The count of 20 in tuple1 is:", tuple1.count(20))
+
+
+
+# practice questions on list and tuple
+
+# movie1 = input("Enter your first favorite movie name:")
+# movie2 = input("Enter your second favorite movie name:")
+# movie3 = input("Enter your third favorite movie name:")
+
+# favorite_movie = [movie1, movie2, movie3]
+# print("Your favorite movies are:", favorite_movie)
+
+
+
+# question no 2: 
+
+palindrome_checker = ["madam", "racecar", "level", "world", "python", "noon"]
