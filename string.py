1706200195d4c69d0cdaf3aa073987b0e74f047f
Nitesh Mishra <niteshmishracs1404@gmail.com>
2025-11-05 10:04:38 +0530

    program

--- string.py
+++ string.py
@@ -28,3 +28,21 @@ if num % 2 == 0 or num == 0:
 
 else:
     print(num, "is an odd number")
+
+
+# gratest of the three numbers
+
+num1 = int(input("Enter first number:"))
+num2 = int(input("Enter second number:"))
+num3 = int(input("Enter third number:"))
+
+if num1 >= num2 and num1 >= num3:
+    print(num1, "is the greatest number")
+
+elif num2 >= num1 and num2 >= num3:
+    print(num2, "is the greatest number")
+
+else:
+    print(num3, "is the greatest number")
+
+    
